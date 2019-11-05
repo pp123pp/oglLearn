960ab9ce878cd8c14c419262314dc81b4635efc7
pp123pp <1217272252@qq.com>
2019-11-05 20:04:23 +0800

    初始化

--- src/extras/Animation.js
+++ src/extras/Animation.js
@@ -20,26 +20,36 @@ export class Animation {
 
     update(totalWeight = 1, isSet) {
         const weight = isSet ? 1 : this.weight / totalWeight;
+
+        //这里计算当前帧所处的整个动画的步长0为下一轮骨骼动画的开始
         const elapsed = this.elapsed % this.duration;
 
+        //向下取整，计算当前帧应该执行哪组骨骼动画
         const floorFrame = Math.floor(elapsed);
         const blend = elapsed - floorFrame;
+        //上一个关键帧动画
         const prevKey = this.data.frames[floorFrame];
+        //下一个应该执行的关键帧动画
         const nextKey = this.data.frames[(floorFrame + 1) % this.duration];
 
+        //遍历每根骨骼
         this.objects.forEach((object, i) => {
+            //该骨骼前一个关键帧的姿态
             prevPos.fromArray(prevKey.position, i * 3)
             prevRot.fromArray(prevKey.quaternion, i * 4);
             prevScl.fromArray(prevKey.scale, i * 3);
 
+            //该骨骼目标关键帧的姿态
             nextPos.fromArray(nextKey.position, i * 3);
             nextRot.fromArray(nextKey.quaternion, i * 4);
             nextScl.fromArray(nextKey.scale, i * 3);
 
+            //这里对姿态进行线性插值，使其进行平滑过渡
             prevPos.lerp(nextPos, blend);
             prevRot.slerp(nextRot, blend);
             prevScl.lerp(nextScl, blend);
 
+            //这里将最终的姿态传给最终的骨骼
             object.position.lerp(prevPos, weight);
             object.quaternion.slerp(prevRot, weight);
             object.scale.lerp(prevScl, weight);
--- src/extras/Skin.js
+++ src/extras/Skin.js
@@ -15,7 +15,9 @@ export class Skin extends Mesh {
     } = {}) {
         super(gl, {geometry, program, mode});
 
+        //创建骨骼
         this.createBones(rig);
+        //创建骨骼的纹理
         this.createBoneTexture();
         this.animations = [];
 
@@ -28,15 +30,20 @@ export class Skin extends Mesh {
     createBones(rig) {
 
         // Create root so that can simply update world matrix of whole skeleton
+        //创建一个根对象，用来更新某个骨骼的世界矩阵
         this.root = new Transform();
         
         // Create bones
         this.bones = [];
         if (!rig.bones || !rig.bones.length) return;
+
+        //遍历每根骨头
         for (let i = 0; i < rig.bones.length; i++) {
+            //每个骨骼是一个对象
             const bone = new Transform();
 
             // Set initial values (bind pose)
+            //设置骨骼的初始姿态
             bone.position.fromArray(rig.bindPose.position, i * 3)
             bone.quaternion.fromArray(rig.bindPose.quaternion, i * 4);
             bone.scale.fromArray(rig.bindPose.scale, i * 3);
@@ -46,20 +53,25 @@ export class Skin extends Mesh {
         
         // Once created, set the hierarchy
         rig.bones.forEach((data, i) => {
+            //为每个骨骼绑定名字
             this.bones[i].name = data.name;
+            //如果parent属性为-1，则为root下的第一级
             if (data.parent === -1) return this.bones[i].setParent(this.root);
             this.bones[i].setParent(this.bones[data.parent]);
         });
 
         // Then update to calculate world matrices
+        //更新root以及子级的所有骨骼的世界矩阵的世界矩阵
         this.root.updateMatrixWorld(true);
 
         // Store inverse of bind pose to calculate differences
         this.bones.forEach(bone => {
+            //这里计算每根骨头的世界矩阵的逆(这里需要使用骨骼矩阵 * 定点矩阵来计算最终的顶点坐标，会导致姿态偏移，因此使用逆矩阵来表示变换后的顶点)
             bone.bindInverse = new Mat4(...bone.worldMatrix).inverse();
         });
     }
 
+    //创建骨骼纹理
     createBoneTexture() {
         if (!this.bones.length) return;
         const size = Math.max(4, Math.pow(2, Math.ceil(Math.log(Math.sqrt(this.bones.length * 4)) / Math.LN2)));
@@ -76,6 +88,7 @@ export class Skin extends Mesh {
     }
 
     addAnimation(data) {
+        //为每个骨骼绑定每帧的姿态
         const animation = new Animation({objects: this.bones, data});
         this.animations.push(animation);
         return animation;
